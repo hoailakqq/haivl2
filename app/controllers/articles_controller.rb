a1dfccda6277ab76dfb9cfe1c0dcb241145a4b71
ThuongHoai <thuonghoai.financial@gmail.com>
2016-03-03 14:35:28 +0700

    show

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,8 +4,8 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    @article = Article.new(params.require(:article).permit(:title, :content))
-    if @article.save
+    @article = Article.new(params.require(:article).permit(:title , :content))
+    if article.save
       redirect_to root_path
     else
       render :new
